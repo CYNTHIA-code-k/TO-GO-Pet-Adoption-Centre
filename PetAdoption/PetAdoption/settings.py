e26f9fcfa81d102b2ff6d0ac483316aab56ac625
cynthia <cyeeyearn@gmail.com>
2025-03-03 14:28:47 +0300

    final final settings

--- PetAdoption/PetAdoption/settings.py
+++ PetAdoption/PetAdoption/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+import dj-database-url
 from dotenv import load_dotenv
 
 load_dotenv()
